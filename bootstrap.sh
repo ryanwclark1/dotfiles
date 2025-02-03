e26d89111d9d0ed0ca5417e5bb1d96bc658608ea
Ryan Clark <ryanc@accentservices.com>
2025-02-03 14:16:25 -0600

    Fix bootstrap.sh to properly terminate zoxide installation check

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,7 +36,7 @@ fi
 if ! command -v zoxide &> /dev/null; then
   echo "Installing zoxide..."
   curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-file
+fi
 
 # Install k9s
 if ! command -v k9s &> /dev/null; then
